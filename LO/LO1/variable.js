42a29bfb144959b67c5641031f6834b0fb8bc81b
dreamrose0515 <dreamrosefruponga@gmail.com>
2021-02-06 08:42:57 +0800

    UC2- TESDA APP.JS
    ASS

---
+++ LO/LO1/variable.js
@@ -0,0 +1,49 @@
+// var, let, const
+
+// var house = 'Stark'
+// console. log(house);
+// house = 'Targaryen'
+
+
+
+// Init var
+// var greeting;
+// console.log(greeting)
+// greeting = 'Hola';
+// console.log(greeting);
+
+// letters, number, _, $
+// Can not start with number
+
+//Multi word vars
+var firstName = 'John'; // Camel case
+var first_name = 'Arya' // Underscore
+var FirstName = 'Khaleesi'; // Pascal case
+
+// Let
+// let name; 
+// name = 'John Snow';
+// console.log(name)
+
+// const
+// const city = 'Caloocan'
+// console.log(city);
+// // Can not reassign
+// name = 'Taguig';
+// console.log(city);
+
+
+const person ={
+    name: 'John',
+    age: 25
+};
+
+person.name = 'Arya';
+
+console.log(person);
+
+const numbers = [1,2,3,4,5];
+
+numbers.push(6);
+
+console.log(numbers);
